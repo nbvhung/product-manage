5d0c1f4efdac624d89e7e0e244bdfdf76aa3d9d6
nguyen ba viet hung <nguyenbaviethung300805@Gmail.com>
2024-09-29 11:34:40 +0700

    initial commit

---
+++ controllers/client/product.controller.js
@@ -0,0 +1,16 @@
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req,res) => {
+    const products = await Product.find({});
+
+    products.forEach(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(2);
+    });
+
+    console.log(products);
+
+    res.render("client/pages/products/index",{
+        pageTitle: "Danh sách sản phẩm",
+        products: products
+    });
+}
